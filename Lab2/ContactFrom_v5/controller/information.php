715e99d29c4f83728354a4b34973b042ff0068cc
bacht <bachgtung144007@gmail.com>
2020-09-29 15:44:40 +0700

    asfasfaf

---
+++ Lab2/ContactFrom_v5/controller/information.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your class address is: <?php echo $_GET["class"]; ?> <br>
+your univercity is <?php echo $_GET["university"]; ?><br>
+hobby <?php echo $_GET["type-product"]; ?><br>
+about  <?php echo $_GET["message"]; ?><br>
+</body>
+</html>
